d1873c4d4097438c3de9969859ffa78ee0ae078e
Haikal <mhaikalas@gmail.com>
2025-05-06 12:27:55 +0700

    upadate database initialisation on main

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"haikal/backend-api/config"
+	"haikal/backend-api/database"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	// load .env
 	config.LoadEnv()
 
+	// inisialisasi database
+	database.InitDB()
+
+	// inisialisasi gin
 	router := gin.Default()
 
+	// membuat route dengan method get
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World!",
